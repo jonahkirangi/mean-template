5cbee2d33212be5a1321a36899b01d7b00c64ee2
Jonah Kirangi <jonahkirangi@gmail.com>
2016-01-06 13:14:26 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var logger = require('morgan');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Express Setup
+app.set('port', process.env.PORT || 3000);
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start Application
+app.listen(app.get('port'), function() {
+  console.log('Server listening on port ' + app.get('port'));
+});

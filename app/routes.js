f1bf1ba044bccdb399823cba838cb7e2778d0752
Jonah Kirangi <jonahkirangi@gmail.com>
2016-01-16 00:33:51 -0800

    Code syntax clean up

--- app/routes.js
+++ app/routes.js
@@ -4,67 +4,67 @@ var Thing = require('../app/models/thing');
 module.exports = function (app, passport) {
 
   // ADD A NEW THING
-  app.post('/api/things', ensureAuthenticated, function(req, res, next) {
+  app.post('/api/things', ensureAuthenticated, function (req, res, next) {
     var thing = new Thing({
       name: req.body.thingName,
       description: req.body.thingDescription,
       createdBy: req.user._id
     });
-    thing.save(function(err) {
+    thing.save(function (err) {
       if (err) return next(err);
       res.sendStatus(200);
     });
   });
 
   // GET ALL THINGS
-  app.get('/api/things', ensureAuthenticated, function(req, res, next) {
+  app.get('/api/things', ensureAuthenticated, function (req, res, next) {
     var query = Thing.find();
 
-    query.exec(function(err, things) {
+    query.exec(function (err, things) {
       if (err) return next(err);
       res.send(things);
     });
   });
 
   // GET A SPECIFIC THING
-  app.get('/api/things/:id', ensureAuthenticated, function(req, res, next) {
-    Thing.findById(req.params.id, function(err, thing) {
+  app.get('/api/things/:id', ensureAuthenticated, function (req, res, next) {
+    Thing.findById(req.params.id, function (err, thing) {
       if (err) return next(err);
       res.send(thing);
     });
   });
 
   // USER LOGIN
-  app.post('/api/login', passport.authenticate('local'), function(req, res) {
+  app.post('/api/login', passport.authenticate('local'), function (req, res) {
     res.cookie('user', JSON.stringify(req.user));
     res.send(req.user);
   });
 
   // USER SIGN UP
-  app.post('/api/signup', function(req, res, next) {
+  app.post('/api/signup', function (req, res, next) {
     var user = new User({
       email: req.body.email,
       password: req.body.password
     });
-    user.save(function(err) {
+    user.save(function (err) {
       if (err) return next(err);
       res.sendStatus(200);
     });
   });
 
   // USER LOGOUT
-  app.get('/api/logout', function(req, res, next) {
+  app.get('/api/logout', function (req, res, next) {
     req.logout();
     res.sendStatus(200);
   });
 
   // HTML5 pushState redirect
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     res.redirect('/#' + req.originalUrl);
   });
 
   // Console log a stack trace on error
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     console.error(err.stack);
     res.status(500).send({ message: err.message });
   });

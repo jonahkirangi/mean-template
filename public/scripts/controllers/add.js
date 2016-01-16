1bd017d0761673a0d567f5ff577492acfb7b8836
Jonah Kirangi <jonahkirangi@gmail.com>
2016-01-16 11:06:59 -0800

    Redirect authenticated user to /home after successful POST to /api/things

--- public/scripts/controllers/add.js
+++ public/scripts/controllers/add.js
@@ -1,11 +1,9 @@
 angular.module('MeanTemplate')
-  .controller('AddCtrl', ['$scope', '$alert', 'Thing', function($scope, $alert, Thing) {
+  .controller('AddCtrl', ['$scope', '$alert', '$location', 'Thing', function($scope, $alert, $location, Thing) {
     $scope.addThing = function() {
       Thing.save({ thingName: $scope.thingName, thingDescription: $scope.thingDescription },
         function() {
-          $scope.thingName = '';
-          $scope.thingDescription = '';
-          $scope.addForm.$setPristine();
+          $location.path('/home');
           $alert({
             content: 'Thing has been added.',
             placement: 'top-right',
